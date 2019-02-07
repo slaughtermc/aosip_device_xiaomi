b26cc83118c759ec922b3c086467e148027cc729
slaughtermc <47281879+slaughtermc@users.noreply.github.com>
2019-02-07 18:43:24 +0000

    Update cos.mk

--- helium/cos.mk
+++ helium/cos.mk
@@ -25,7 +25,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/helium/device.mk)
 
 # Inherit some common aosip stuff.
-$(call inherit-product, vendor/cos/config/common_full_phone.mk)
+$(call inherit-product, vendor/cos/common.mk)
 
 # Set those variables here to overwrite the inherited values.
 BOARD_VENDOR := Xiaomi
